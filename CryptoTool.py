49167744b09bc0e0837df4b2be3ab6303bca891f
sa-blip213 <sagarbibek231@gmail.com>
2025-05-25 03:17:48 +0545

    Create CryptoTool.py
    Initial Commit

---
+++ CryptoTool.py
@@ -0,0 +1,6 @@
+class CryptoTool:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Encryption/Decryption and Hashing Tool")
+        self.root.geometry("600x400")
+      
